e532e768bfe5874fe8c0c2240ce8b2aba153261e
atitsbest <atitsbest.developer@gmail.com>
2013-02-02 18:56:44 +0100

    + Startseite angefangen.

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Llprk.Web.UI.Models;
+using Llprk.Web.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Llprk.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Entities db = new Entities();
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(new HomeIndex() { 
+                Products = db.Products.Take(4)
+            });
+        }
+
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
