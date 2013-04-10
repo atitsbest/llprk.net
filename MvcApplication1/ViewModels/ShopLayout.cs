135b5abaab2a6352090509cdb3d808a9d45646bf
atitsbest <atitsbest@gmail.com>
2013-04-10 17:42:40 +0200

    + ShowYourLove mit weißen Hintergrund.

---
+++ MvcApplication1/ViewModels/ShopLayout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Llprk.Web.UI.Models;
+
+namespace Llprk.Web.UI.ViewModels
+{
+    /// <summary>
+    /// ViewModel für ~/shop/index
+    /// </summary>
+    public class ShopLayout
+    {
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
