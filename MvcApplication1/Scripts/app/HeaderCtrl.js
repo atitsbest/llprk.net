00903159acc0c9e23247ffaa858a8c1f35c310d4
atitsbest <atitsbest@gmail.com>
2013-04-11 16:05:49 +0200

    + Warenkorb wird jetzt gelossen wenn irgendwo außerhalb geklickt wird.

---
+++ MvcApplication1/Scripts/app/HeaderCtrl.js
@@ -0,0 +1,25 @@
+﻿/**
+ */
+function HeaderCtrl($scope) {
+	/**
+	 * Warenkorb anzeigen.
+	 */
+	$scope.showCart = function () {
+		// Backdrop einfügen.
+		$('body').append('<div class="modal-backdrop" style="opacity:.1;cursor:pointer;"></div>');
+		$('#cart').show();
+		$('.modal-backdrop').click(function () {
+			$scope.hideCart();
+		});
+	}
+
+	/**
+	 * Warenkorb wieder ausblenden.
+	 */
+	$scope.hideCart = function () {
+		$('#cart').hide();
+		$('.modal-backdrop').remove();
+	}
+}
+
+HeaderCtrl.$inject = ["$scope"];
